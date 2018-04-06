4729b4aded623675e6165f902d48afc1f497ccc5
Alienah <aidalbla@gmail.com>
2018-04-06 18:32:38 +0200

    Select all funtion

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const dishTitle = document.querySelector('.title--dish');
 let listHTML = '';
 const ingredientsList = document.querySelector('.items-list');
 
+
 fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json')
 .then(function(response){
   return response.json();
@@ -10,6 +11,7 @@ fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-seta
 .then(function(json) {
   const recipe = json.recipe;
   const ingredients = recipe.ingredients;
+
   dishTitle.innerHTML = recipe.name;
 
   for (let i = 0; i < ingredients.length; i++) {
@@ -18,7 +20,7 @@ fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-seta
       `<li class="item">
         <div class="item--inside">
           <div class="item--input">
-            <input type="checkbox" name="item-selected" value="">
+            <input type="checkbox" name="item-selected" value="${ingredients[i].price}">
           </div>
           <div class="item--input">
             <input type="text" class="input--quantity" value="">
@@ -32,5 +34,36 @@ fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-seta
         </div>
       </li>`
     ingredientsList.insertAdjacentHTML('beforeend', item);
+
+    // const itemValue = document.getElementById('item-selected').value;
+    // console.log(itemValue);
+    // document.getElementById('item-selected').addEventListener('click',sumItemSelected);
   }
+
+
+
 })
+.catch(function(error){
+  console.log(error);
+})
+
+function checkAll(e){
+  e.preventDefault();
+	const items = document.getElementsByName('item-selected');
+	for(let i=0; i<items.length; i++){
+		if(items[i].type=='checkbox')
+		items[i].checked=true;
+	}
+}
+
+function uncheckAll(e){
+  e.preventDefault();
+	const items = document.getElementsByName('item-selected');
+	for(let i=0; i<items.length; i++){
+		if(items[i].type=='checkbox')
+		items[i].checked=false;
+	}
+}
+
+document.querySelector('.check-all').addEventListener('click', checkAll);
+document.querySelector('.uncheck-all').addEventListener('click', uncheckAll);
